4864212373d11372235e4e6337fec9022a7c4fd1
erniemel <erniemel@ts137-pc5.student.yo.oulu.fi>
2017-10-05 14:49:58 +0300

    Assignment completed

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,85 @@
 
+
 public class SudokuVerifier {
 	
-	public int verify(String candidateSolution) {
+	public static int sudokuLength = 81;
+	
+	public int verify(String solution) {
+		if (!checkLength(solution) || !checkInteger(solution)) {
+			return -1;
+		}
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
 		//check
+	}
+	
+	public boolean checkLength(String solution) {
+		if (solution.length() == sudokuLength) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean checkInteger(String solution) {
+		try { 
+		    Integer.parseInt(solution); 
+		} catch(NumberFormatException e) { 
+			return false; 
+		} catch(NullPointerException e) {
+		    return false;
+		}
+		int i = Integer.parseInt(solution);
+		if ((i > 0) && !(solution.contains("0"))) {
+		    return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean checkRows(String solution) {
+		int index;	
+		int rowCounter = 0;
+		while (rowCounter < 9) {
+			index = 1;
+			String row = solution.substring(rowCounter*9, rowCounter*9 + 9);
+			for (char c: row.toCharArray()) {
+				if (row.substring(index).contains(""+c)) {
+					return false;
+				}
+				index++;
+			}
+			rowCounter++;
+		}		
+		return true;
+	}
+	
+	public boolean checkColumns(String solution) {
+		int rowIndex = 0;	
+		int columnIndex = 0;
+		int substrIndex = 1;
+		String column = "";
+		String substr = "";
+		while (columnIndex < 9) {
+			while (rowIndex < 9) {
+				substr = solution.substring(rowIndex*9 + columnIndex, rowIndex*9 + columnIndex + 1);
+				column += substr;
+				rowIndex++;
+			}
+			for (char c: column.toCharArray()) {
+				System.out.println(c);
+				System.out.println(column.substring(substrIndex));
+				/*if (column.substring(substrIndex).charAt(c) > 0) {
+					return false;
+				} else {*/
+					substrIndex++;
+				//}
+			}
+			columnIndex++;
+		}
+
+		
+		
+		return true;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,68 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void checkLengthIs81() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertTrue(sudoku.checkLength("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+	}
+	
+	@Test
+	public void checkLengthLessThan81() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertFalse(sudoku.checkLength("41736982563215894795872431682543716979158643234691275828964357157329168416487529"));
 	}
 
+	@Test
+	public void checkLengthMoreThan81() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertFalse(sudoku.checkLength("4173698256321589479587243168254371697915864323469127582896435715732916841648752931"));
+	}
+	
+	@Test
+	public void checkSudokuLength() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertEquals(sudoku.sudokuLength, 81);
+	}
+	
+	@Test
+	public void checkContainsZero() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertFalse(sudoku.checkInteger("417369825632158947958724316825437169791586432346012758289643571573291684164875293"));
+	}
+	
+	@Test
+	public void checkContainsLetter() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertFalse(sudoku.checkInteger("417369825632158947958724316825437169791586432346a12758289643571573291684164875293"));
+	}
+	
+	@Test
+	public void checkIsNegative() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertFalse(sudoku.checkInteger("-17369825632158947958724316825437169791586432346012758289643571573291684164875293"));
+	}
+	
+	@Test
+	public void checkIncorrectRow() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertFalse(sudoku.checkRows("447369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+	}
+	
+	@Test
+	public void checkCorrectRow() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertTrue(sudoku.checkRows("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+	}
+	
+	@Test
+	public void checkIncorrectColumn() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertFalse(sudoku.checkColumns("417369825632158947958724316825437169791586432346912758289643571573291683164875293"));
+	}
+	
+	@Test
+	public void checkCorrectColumn() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertTrue(sudoku.checkColumns("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+	}
 }
